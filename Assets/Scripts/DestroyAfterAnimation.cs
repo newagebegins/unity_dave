c088f69a25a74253d37ed4fde93de5179cb3d1d2
newagebegins <newagebegins@gmail.com>
2015-06-21 11:28:59 +1000

    Add DestroyAfterAnimation, remove DestroyScript

---
+++ Assets/Scripts/DestroyAfterAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DestroyAfterAnimation : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
